a5ce297242fc54651ba2cf2efb16fd5a3d42e78a
Ian Kamar <ian.kamar@outlook.com>
2023-09-09 08:36:34 -0400

    update my project

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   console.log("footer");
   return (
     <footer className="footer">
-      <div>Developed by Kamar</div>
+      <div>Developed by Ian Kamar</div>
       <div>2023</div>
     </footer>
   );
