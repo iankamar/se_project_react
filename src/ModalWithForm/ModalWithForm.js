8ac4dcb8d9db7a995ae5b00185167fb8726f241c
Ian Kamar <ian.kamar@outlook.com>
2023-09-09 02:42:58 -0400

    update my project

--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -15,7 +15,7 @@ const ModalWithForm = ({
           <button type="button" className="modal__close" onClick={onClose}>
             &#x2715;
           </button>
-          <h3 className="modal__title"> {title}</h3>
+          <h3 className="modal__title">{title}</h3>
         </div>
         <div className="modal__fieldset">
           <form className="modal__form">{children}</form>
