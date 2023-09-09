cc3b715e73d6a69f64f433e2101cdac62a3d7c31
Ian Kamar <ian.kamar@outlook.com>
2023-09-09 08:21:29 -0400

    update my project

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -7,8 +7,8 @@ const ItemModal = ({ selectedCard, onClose }) => {
         <button type="button" onClick={onClose}>
           Close
         </button>
-        <img src={selectedCard.link} />
-        <div> {selectedCard.name}</div>
+        <img src={selectedCard.link} alt="SelectedCard" />
+        <h3> {selectedCard.name}</h3>
         <div>Weather type: {selectedCard.weather}</div>
       </div>
     </div>
