508c857868325974e3abdaac38d10d03440b0c28
Ian Kamar <ian.kamar@outlook.com>
2023-09-18 12:56:18 -0400

    update my project

--- src/components/Profile/ClothesSection/ClothesSection.js
+++ src/components/Profile/ClothesSection/ClothesSection.js
@@ -1,12 +1,45 @@
+/*
 import React from "react";
 import ItemCard from "../../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../../utils/Constants";
+import "./ClothesSection.css";
 
-const ClothingSection = ({ onSelectCard, onDeleteCard }) => {
+const ClothingSection = ({ onSelectCard, onDeleteCard, onAddNew }) => {
   return (
     <div className="clothes__section">
+      <h1>Clothing Section</h1>
       <h2>Your items</h2>
-      <div className="card_items">
+      <button onClick={onAddNew}>+ Add New</button>
+      <div className="clothes__items">
+        {defaultClothingItems.map((item) => (
+          <ItemCard
+            key={item._id}
+            item={item}
+            onSelectCard={onSelectCard}
+            onDeleteCard={onDeleteCard}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ClothingSection; */
+
+import React from "react";
+import ItemCard from "../../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../../utils/Constants";
+import "./ClothesSection.css";
+
+const ClothingSection = ({ onSelectCard, onDeleteCard, onAddNew }) => {
+  return (
+    <div className="clothes__section">
+      <h1>Clothing Section</h1>
+      <div className="header__wrapper">
+        <h2>Your items</h2>
+        <button onClick={onAddNew}>+ Add New</button>
+      </div>
+      <div className="clothes__items">
         {defaultClothingItems.map((item) => (
           <ItemCard
             key={item._id}
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import avatar from "../../images/avatar.svg";
 import { Link } from "react-router-dom";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/Constants";
 import "./Profile.css";
+import ItemModal from "../ItemModal/ItemModal";
 
 const Profile = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  const onSelectCard = (item) => {
+    setSelectedCard(item);
+  };
+
+  const onClose = () => {
+    setSelectedCard(null);
+  };
+
   return (
-    <div className="profile__avatar">
-      <div>
-        <img src={avatar} alt="avatar" />{" "}
-      </div>
-      <div>
-        <Link to="/profile">Name</Link>
+    <div className="profile">
+      <div className="sidebar">{/* Sidebar content goes here */}</div>
+      <div className="profile__content">
+        <div className="profile__avatar">
+          <div>
+            <img src={avatar} alt="avatar" />
+          </div>
+          <div>
+            <Link to="/profile">Name</Link>
+          </div>
+        </div>
+        <div className="clothes__section">
+          <h2>Your items</h2>
+          <div className="card__items">
+            {defaultClothingItems.map((item) => (
+              <ItemCard
+                key={item._id}
+                item={item}
+                onSelectCard={() => onSelectCard(item)}
+              />
+            ))}
+          </div>
+        </div>
+        {selectedCard && (
+          <ItemModal
+            selectedCard={selectedCard}
+            onClose={onClose}
+            // Pass your onDeleteItem function here
+          />
+        )}
       </div>
     </div>
   );
