b0538ea9b5a0d7c23074663858e79c432f33ced7
Ian Kamar <ian.kamar@outlook.com>
2023-09-19 09:49:17 -0400

    update my project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,10 @@
-/*
-import React from "react";
-import logo from "../../images/logo.svg";
-import avatar from "../../images/avatar.svg";
-import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
-import { Link } from "react-router-dom";
-import "./Header.css";
-
-const Header = ({ onCreateModal }) => {
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <div>
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-        </div>
-        <div>Date</div>
-      </div>
-      <div className="header__avatar-logo">
-        <ToggleSwitch />
-        <div>
-          <button type="text" onClick={onCreateModal} className="header__item">
-            {" "}
-            + Add New Clothes{" "}
-          </button>
-        </div>
-        <Link to="/profile">Name</Link>
-        <div>
-          <img src={avatar} alt="avatar" />{" "}
-        </div>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
-*/
 import React, { useState, useEffect } from "react";
 import logo from "../../images/logo.svg";
 import avatar from "../../images/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import { getForecastWeather } from "../../utils/weatherAPi"; // Import your API function
+import { getForecastWeather } from "../../utils/weatherAPi";
 
 const Header = ({ onCreateModal }) => {
   const [date, setDate] = useState(new Date());
