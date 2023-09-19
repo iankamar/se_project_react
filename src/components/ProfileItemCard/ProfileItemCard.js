1b5f1011ca3285bb71d0d4f65d4efe527f0d59e9
Ian Kamar <ian.kamar@outlook.com>
2023-09-19 05:05:43 -0400

    update my project

--- src/components/ProfileItemCard/ProfileItemCard.js
+++ src/components/ProfileItemCard/ProfileItemCard.js
@@ -3,16 +3,14 @@ import "./ProfileItemCard.css";
 
 const ProfileItemCard = ({ item, onSelectCard, onDeleteCard }) => {
   return (
-    <div>
-      <h3 className="card_name"> {item.name} </h3>
-      <div>
-        <img
-          src={item.link}
-          alt={item.name}
-          className="card__image"
-          onClick={() => onSelectCard(item)}
-        />
-      </div>
+    <div className="card">
+      <img
+        src={item.link}
+        alt={item.name}
+        className="card__image"
+        onClick={() => onSelectCard(item)}
+      />
+      <h3 className="card__name"> {item.name} </h3>
     </div>
   );
 };
