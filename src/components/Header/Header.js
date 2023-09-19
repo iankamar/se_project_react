72de179fd5288e30da65fb125d25c697db6c5756
Ian Kamar <ian.kamar@outlook.com>
2023-09-19 08:06:21 -0400

    update my project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import logo from "../../images/logo.svg";
 import avatar from "../../images/avatar.svg";
@@ -15,6 +16,54 @@ const Header = ({ onCreateModal }) => {
           </Link>
         </div>
         <div>Date</div>
+      </div>
+      <div className="header__avatar-logo">
+        <ToggleSwitch />
+        <div>
+          <button type="text" onClick={onCreateModal} className="header__item">
+            {" "}
+            + Add New Clothes{" "}
+          </button>
+        </div>
+        <Link to="/profile">Name</Link>
+        <div>
+          <img src={avatar} alt="avatar" />{" "}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+*/
+import React, { useState, useEffect } from "react";
+import logo from "../../images/logo.svg";
+import avatar from "../../images/avatar.svg";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { getForecastWeather } from "../../utils/weatherAPi"; // Import your API function
+
+const Header = ({ onCreateModal }) => {
+  const [date, setDate] = useState(new Date());
+  const [cityName, setCityName] = useState("");
+
+  useEffect(() => {
+    getForecastWeather().then(({ cityName }) => setCityName(cityName));
+  }, []);
+
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <div>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div>
+          {date.toLocaleDateString("en-US", { month: "long", day: "numeric" })},{" "}
+          {cityName}
+        </div>
       </div>
       <div className="header__avatar-logo">
         <ToggleSwitch />
--- src/utils/weatherAPi.js
+++ src/utils/weatherAPi.js
@@ -1,32 +1,36 @@
 // https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}//
 
-const latitude = 44.34;
-const longitude = 10.99;
+const latitude = 38.93;
+const longitude = -77.18;
 const APIkey = "37fd47fdc6ac7caa0dde7c4c82f0be18";
 
 export const getForecastWeather = () => {
   const weatherApi = fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Error: ${res.status}`);
+      }
+    })
+    .then((data) => {
+      const cityName = data.name;
+      data.cityName = cityName;
+      return data;
+    });
   return weatherApi;
 };
 
 export const parseWeatherData = (data) => {
   const main = data.main;
   const temperature = main && main.temp;
-  /*console.log(Math.ceil(temperature));*/
   const weather = {
     temperature: {
       F: Math.round(temperature),
       C: Math.round(((data.main.temp - 32) * 5) / 9),
     },
   };
-  /*console.log(weather);*/
   return Math.ceil(temperature);
 };
