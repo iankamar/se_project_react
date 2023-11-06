f5d7d5cd4113fb007f6a0351c0889ada56d6b048
Ian Kamar <ian.kamar@outlook.com>
2023-11-05 19:53:55 -0500

    update my project

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -1,36 +1,3 @@
-/*
-
-const LoginModal = ({ onLogin }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onLogin({ email, password });
-  };
-
-  return (
-    <ModalWithForm title="Login" onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-    </ModalWithForm>
-  );
-};
-
-export default LoginModal; */
-
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
